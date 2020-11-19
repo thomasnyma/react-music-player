24c9493814f00b033170983dd75d05d1f4518095
Thomas Nyholm Maneschijn <thomasmaneschijn@gmail.com>
2020-11-19 13:34:59 +0100

    Basic components

--- react-music-player/src/App.js
+++ react-music-player/src/App.js
@@ -1,9 +1,14 @@
-
+// Styles
+import './styles/app.scss';
+// Components
+import Player from './components/Player';
+import Song from './components/Song';
 
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
